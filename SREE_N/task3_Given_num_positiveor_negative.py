6e20d49eb1a202964f80de36a6a4acf08de2d467
sreehari.nagineni <sreehari.nagineni@outlook.com>
2022-04-23 08:04:24 +0530

    Added assignment files 1 & 2 & 3 & 4 & 5

---
+++ SREE_N/task3_Given_num_positiveor_negative.py
@@ -0,0 +1,6 @@
+#3. WAP to check whether a given number is positive or negative
+a=int(input("enter a value:"))
+if a>=0:
+    print("value is POSSITIVE")
+else:
+    print("value is NEGATIVE")
---
+++ SREE_N/task5_Eligible_vote.py
@@ -0,0 +1,6 @@
+#5. WAP to read the age of a candidate and determine whether it is eligible for casting his/her own vote.
+age=int(input("enter AGE:"))
+if age>=18:
+    print("your age eligible for vote")
+else:
+    print("your age not eligible for vote")
